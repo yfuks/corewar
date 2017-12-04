460d753ebc8c114de9499d52f3126f8e61460ffb
Ayme LANSIVAL <alansiva@e3r3p18.42.fr>
2017-12-04 13:57:56 +0100

    add scirpt for test asm

---
+++ test_asm/test_tab.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+for champion in `ls *.s`
+do
+	echo _____________________________________________
+	echo Champion : $champion
+	echo "____ O U R      A S M ____"
+	./$1 $champion
+done
+
+mv *.cor asm_our
+
+for champion in `ls *.s`
+do
+	echo _____________________________________________
+	echo Champion : $champion
+	echo "____ Z A Z      A S M ____"
+	./$2 $champion
+done
+
+mv *.cor asm_zaz/
+
+for champion in `ls asm_our/*.cor`
+do
+	hexdump $champion > $champion.txt
+done
+
+mv asm_our/*.txt diff
+
+for champion in `ls asm_zaz/*.cor`
+do
+	hexdump $champion > $champion-zaz.txt
+done
+
+mv asm_zaz/*.txt diff
+
+i=0
+j=0
+
+nb_diff=($(ls diff | wc -w))
+let 'nb_diff = nb_diff / 2'
+
+array=($(ls diff/*.txt))
+
+for j in `seq 1 $nb_diff`
+do
+	diff -q ${array[i]} ${array[i+1]}
+	let 'i += 2'
+done
