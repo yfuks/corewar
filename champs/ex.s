53247cf97429809b63cf99b13772465d6be0f0e8
yfuks <yfuks@student.42.fr>
2017-12-13 14:21:34 +0100

    Update fuks zork to killlllll

--- champs/ex.s
+++ champs/ex.s
@@ -3,10 +3,22 @@
 		
 		sti r1, %:live, %1
 		sti r1, %:live2, %1
-		ld	%0, r2
+		ld :live2, r6
+		ld 	%22, r7
+		ld	%238, r4
+		ld :end, r3
+		ld :sti, r5
+
 live:	live %0
-		fork %:live2
 		zjmp %:live
-live2:	live %0
-		zjmp %:live2
+
+		fork %:live2
+		ld	%0, r2
+		zjmp %:live
+
+sti:	sti r5, r7, %0
 	
+live2:	live %0
+		sti r6, r4, r4
+end:	sti r5, r3, r7
+
